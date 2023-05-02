43cfa16e32962d97f4839b3475a92a52b8154e36
Chanakya Sarma <chanakyasarmakashyap@gmail.com>
2023-05-02 13:09:47 +0530

    1 commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <nav className='flex justify-between items-center w-full mb-10 pt-5'>
         <img src={logo} alt="sumz_logo"
         className='w-28 object-contain h-20'></img>
-        <button type='button' onClick={() => window.open('')}
+        <button type='button' onClick={() => window.open('https://github.com/Chanakyasarma/Summary-gpt')}
         className='black_btn h-10'>GitHub</button>
 
       </nav>
@@ -17,7 +17,7 @@ const Hero = () => {
         <span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
       <h3 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
+        Simplify your reading with Summary-GPT, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h3>
     </header>
